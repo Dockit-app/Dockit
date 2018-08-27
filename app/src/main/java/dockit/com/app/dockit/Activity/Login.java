7f35bc45b6bf22415e383af2d04fd4d4eba5da7b
crisnreis <crisnreis@gmail.com>
2018-08-27 15:22:14 +0100

    Login improvements

--- app/src/main/java/dockit/com/app/dockit/Activity/Login.java
+++ app/src/main/java/dockit/com/app/dockit/Activity/Login.java
@@ -1,9 +1,12 @@
 package dockit.com.app.dockit.Activity;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import dockit.com.app.dockit.ViewModel.UserViewModel;
@@ -13,11 +16,15 @@ import dockit.com.app.dockit.R;
 public class Login extends AppCompatActivity {
 
     Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, bEnter;
+    String pinView = "";
+    TextView textPin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textPin = findViewById(R.id.pinEntered);
 
         //Declare all buttons and values in string variables
         b1 = findViewById(R.id.button1);
@@ -56,28 +63,46 @@ public class Login extends AppCompatActivity {
         userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
 
         //on click, add the value of the button the the string pin
-        b1.setOnClickListener(v -> userViewModel.updatePin(button1));
-        b2.setOnClickListener(v -> userViewModel.updatePin(button2));
-        b3.setOnClickListener(v -> userViewModel.updatePin(button3));
-        b4.setOnClickListener(v -> userViewModel.updatePin(button4));
-        b5.setOnClickListener(v -> userViewModel.updatePin(button5));
-        b6.setOnClickListener(v -> userViewModel.updatePin(button6));
-        b7.setOnClickListener(v -> userViewModel.updatePin(button7));
-        b8.setOnClickListener(v -> userViewModel.updatePin(button8));
-        b9.setOnClickListener(v -> userViewModel.updatePin(button9));
-        b0.setOnClickListener(v -> userViewModel.updatePin(button0));
+        b1.setOnClickListener(v -> updatePin(button1));
+        b2.setOnClickListener(v -> updatePin(button2));
+        b3.setOnClickListener(v -> updatePin(button3));
+        b4.setOnClickListener(v -> updatePin(button4));
+        b5.setOnClickListener(v -> updatePin(button5));
+        b6.setOnClickListener(v -> updatePin(button6));
+        b7.setOnClickListener(v -> updatePin(button7));
+        b8.setOnClickListener(v -> updatePin(button8));
+        b9.setOnClickListener(v -> updatePin(button9));
+        b0.setOnClickListener(v -> updatePin(button0));
 
         //when the user press enter, compare pin with the database
-        bEnter.setOnClickListener(v -> userViewModel.doLogin());
+        bEnter.setOnClickListener(v -> userViewModel.doLogin(pinView));
 
         //If toast is an error, make Toast
         userViewModel.toast().observe(this, (String toast) -> {
             assert toast != null;
             if (toast.equals("Wrong Pin!")) {
-                Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_SHORT).show();
+                pinView = "WRONG PIN";
+                textPin.setText(pinView);
+            } else if (toast.equals("Success!")){
+                //Send to the next Activity
+                Intent i = new Intent(this, TableSelection.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                this.startActivity(i);
             }
         });
 
 
     }
+
+    //Update the pin adding the number pressed by the user
+    public void updatePin(String number){
+        if(pinView.equals("WRONG PIN")){
+            pinView = "";
+            textPin.setText(pinView);
+        }
+        pinView = pinView + number;
+        textPin.setText(pinView);
+    }
+
+
 }
--- app/src/main/java/dockit/com/app/dockit/ViewModel/UserViewModel.java
+++ app/src/main/java/dockit/com/app/dockit/ViewModel/UserViewModel.java
@@ -1,15 +1,11 @@
 package dockit.com.app.dockit.ViewModel;
 
-import android.annotation.SuppressLint;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
-import android.content.Context;
-import android.content.Intent;
 import java.util.List;
 
-import dockit.com.app.dockit.Activity.TableSelection;
 import dockit.com.app.dockit.Entity.User;
 import dockit.com.app.dockit.Repository.UserRepository;
 import dockit.com.app.dockit.Tasks.ResultHandler;
@@ -17,18 +13,22 @@ import dockit.com.app.dockit.Tasks.SharedPreferencesManager;
 
 public class UserViewModel extends AndroidViewModel implements ResultHandler<List<User>> {
 
-    private UserRepository mRepository;
     //integration Omnivore -> private OmnivoreUserRepository mOmniRepository;
-    private String pinView = "";
-    @SuppressLint("StaticFieldLeak")
-    private Context context;
+    private UserRepository mRepository;
     private MutableLiveData<String> toast;
 
     public UserViewModel(Application application) {
         super(application);
         mRepository = new UserRepository(application);
         //integration Omnivore -> mOmniRepository = new OmnivoreUserRepository(application);
-        this.context = application;
+    }
+
+    //Try to find username and ID by pin
+    public void doLogin(String pinView){
+        mRepository.comparePin(pinView, this);
+        //integration Omnivore -> mOmniRepository.refreshDatabase();
+
+
     }
 
     //Set the default Toast
@@ -40,38 +40,16 @@ public class UserViewModel extends AndroidViewModel implements ResultHandler<Lis
         return toast;
     }
 
-    //Update the pin adding the number pressed by the user
-    public void updatePin(String number){
-        pinView = pinView + number;
-    }
-
-    //Try to find username and ID by pin
-    public void doLogin(){
-        mRepository.comparePin(pinView, this);
-        //integration Omnivore -> mOmniRepository.refreshDatabase();
-
-
-    }
-
-
     @Override
     public void onResult(List<User> result) {
 
         //If the result isn't empty, save user name and id on Shared Preference. Else, user can try again
         if (!result.toString().equals("[]")) {
-
-            SharedPreferencesManager.getInstance(this.context).userLogin(result);
+            SharedPreferencesManager.getInstance(getApplication()).userLogin(result);
             toast.postValue("Success!");
-
-            //Send to the next Activity
-            Intent i = new Intent(context, TableSelection.class);
-            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(i);
         }
         else {
             toast.postValue("Wrong Pin!");
-            pinView = "";
-
         }
     }
 }
